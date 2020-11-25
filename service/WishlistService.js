ffa9d9a1bae8e3e847d89baeb54c4c15f0b3efce
윤희성 <heesung6701@naver.com>
2020-11-25 20:05:47 +0900

    WishList DAO 구현 및 테스트

--- service/WishlistService.js
+++ service/WishlistService.js
@@ -1,17 +1,59 @@
 'use strict';
 
+const wishlistDao = require('../dao/WishlistDao.js');
 
 /**
  * add wishlist
  * 위시 리스트에 추가하기
  *
- * userIdx Long 유저 ID
- * body User Created user object
+ * body Wishlist Created wishlist object
  * no response value expected for this operation
  **/
-exports.createWishList = function(userIdx,body) {
-  return new Promise(function(resolve, reject) {
-    resolve();
-  });
-}
+exports.createWishlist = ({perfumeIdx, userIdx, priority}) => {
+  return wishlistDao.create({perfumeIdx, userIdx, priority});
+};
 
+
+/**
+ * update wishlist
+ * 위시 리스트에 수정하기
+ *
+ * body Wishlist Created wishlist object
+ * no response value expected for this operation
+ **/
+exports.updateWishlist = ({perfumeIdx, userIdx, priority}) => {
+  return wishlistDao.update({perfumeIdx, userIdx, priority});
+};
+
+/**
+ * delete wishlist
+ * 위시 리스트에 삭제하기
+ *
+ * body Wishlist Created wishlist object
+ * no response value expected for this operation
+ **/
+exports.deleteWishlist = ({perfumeIdx, userIdx}) => {
+  return wishlistDao.delete({perfumeIdx, userIdx});
+};
+
+/**
+ * read user's wishlist
+ * 유저의 위시 리스트에 조회하기
+ *
+ * userIdx Long 유저 ID
+ * no response value expected for this operation
+ **/
+exports.readWishlistByUser = (userIdx) => {
+  return wishlistDao.readByUserIdx(userIdx)
+};
+
+/**
+ * delete user's wishlist
+ * 유저가 가지고 있는 위시 리스트에 전체 삭제하기
+ *
+ * userIdx Long 유저 ID
+ * no response value expected for this operation
+ **/
+exports.deleteWishlistByUser = (userIdx) => {
+  return wishlistDao.deleteByUserIdx(userIdx);
+};
