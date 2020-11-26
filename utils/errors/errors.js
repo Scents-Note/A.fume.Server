8944fe58ca7835f56ea1467ec1fb8c4e2276d9a2
윤희성 <heesung6701@naver.com>
2020-11-26 23:34:34 +0900

    jwt 모듈 구현
    jwt 책임을 가지는 모듈 구현

--- utils/errors/errors.js
+++ utils/errors/errors.js
@@ -60,8 +60,34 @@ class FailedToCreateError extends Error {
     }
 }
 
+class InvalidTokenError extends Error {
+    constructor(code = 'GENERIC', status = statusCode.UNAUTHORIZED, ...params) {
+        super(...params);
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, InvalidTokenError);
+        }
+        this.code = code;
+        this.status = status;
+        this.message = '유효하지 않는 토큰입니다.';
+    }
+}
+
+class ExpiredTokenError extends Error {
+    constructor(code = 'GENERIC', status = statusCode.UNAUTHORIZED, ...params) {
+        super(...params);
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, ExpiredTokenError);
+        }
+        this.code = code;
+        this.status = status;
+        this.message = '만료된 토큰입니다.';
+    }
+}
+
 module.exports.DatabaseError = DatabaseError;
 module.exports.NoReferencedRowError = NoReferencedRowError;
 module.exports.DuplicatedEntryError = DuplicatedEntryError;
 module.exports.NotMatchedError = NotMatchedError;
 module.exports.FailedToCreateError = FailedToCreateError;
+module.exports.InvalidTokenError = InvalidTokenError;
+module.exports.ExpiredTokenError = ExpiredTokenError;
