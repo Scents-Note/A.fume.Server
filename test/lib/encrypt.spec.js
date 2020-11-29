4f673c9fc41fa6afc23abf808096bb11f6ee4666
윤희성 <heesung6701@naver.com>
2020-11-29 12:29:53 +0900

    encrypt module 구현

---
+++ test/lib/encrypt.spec.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv');
+dotenv.config({
+    path: './config/.env.test'
+});
+
+const chai = require('chai');
+const { expect } = chai;
+const { encrypt, decrypt } = require('../../lib/encrypt.js');
+
+describe('# encrypt and decrypt Test', () => {
+    it(' # success case', async () => {
+        const origin = 'test';
+        const hashed = encrypt(origin);
+        const result = await decrypt(hashed);
+        expect(result).to.eq(origin);
+    });
+});
