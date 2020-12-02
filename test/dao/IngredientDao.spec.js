5c6e9be7033859f9298ccc8a7a43c51558601886
yejin <jinee0717@daum.net>
2020-12-02 22:37:43 +0900

    [CONTROLLER, SERVICE] ingredient contoroller, service 구현
    ingredient controller, service 작업
    DAO 변경에 따른 테스트 코드 수정

--- test/dao/IngredientDao.spec.js
+++ test/dao/IngredientDao.spec.js
@@ -13,14 +13,14 @@ const pool = require('../../utils/db/pool.js');
 describe('# ingredientDao Test', () => {
     describe(' # create Test', () => {
         // 중복 데이터 미리 삭제
-        let series_idx
+        let seriesName
         before(async () => {
-            series_idx = (await pool.queryParam_Parse("SELECT series_idx FROM series"))[0].series_idx;
+            seriesName = (await pool.queryParam_Parse("SELECT name FROM series"))[0].name;
             await pool.queryParam_None("DELETE FROM ingredient WHERE name = '테스트 데이터'");
         });
         // 성공 케이스
         it(' # success case', (done) => {
-            ingredientDao.create({name : '테스트 데이터', english_name : "Test Data", description : "왈라왈라", series_idx}).then((result) => {
+            ingredientDao.create({name : '테스트 데이터', englishName : "Test Data", description : "왈라왈라", seriesName}).then((result) => {
                 expect(result).gt(0)
                 done();
             }).catch((err) => {
@@ -30,7 +30,7 @@ describe('# ingredientDao Test', () => {
         });
         // 중복 데이터 발생 케이스
         it(' # DuplicatedEntryError case', (done) => {
-            ingredientDao.create({name : '테스트 데이터', english_name : "Test Data", description : "왈라왈라", series_idx}).then((result) => {
+            ingredientDao.create({name : '테스트 데이터', englishName : "Test Data", description : "왈라왈라", seriesName}).then((result) => {
                 expect(false).true();
                 done();
             }).catch((err) => {
@@ -45,14 +45,14 @@ describe('# ingredientDao Test', () => {
     });
 
     describe(' # read test', () =>{
-        let read, ingredient_idx, name
+        let read, ingredientIdx, name
         before(async() => {
             read = (await pool.queryParam_Parse("SELECT ingredient_idx, name FROM ingredient"))[0];
-            ingredient_idx = read.ingredient_idx
+            ingredientIdx = read.ingredient_idx
             name = read.name
         });
         it(' # success case', (done) => {
-            ingredientDao.read(ingredient_idx).then((result) => {
+            ingredientDao.read(ingredientIdx).then((result) => {
                 expect(result.name).eq(name);
                 done();
             }).catch((err) => {
@@ -75,14 +75,14 @@ describe('# ingredientDao Test', () => {
     });
 
     describe('# update Test', () => {
-        let ingredient_idx;
+        let ingredientIdx;
         before(async () => {
             await pool.queryParam_None("DELETE FROM ingredient WHERE name='테스트 데이터'");
             const result = await pool.queryParam_None("INSERT INTO ingredient(name, english_name) VALUES('테스트 데이터','Test Data')");
-            ingredient_idx = result.insertId;
+            ingredientIdx = result.insertId;
         });
         it('# success case', (done) => {
-            ingredientDao.update({ingredient_idx, name:'수정 데이터', english_name:'Update Data'})
+            ingredientDao.update({ingredientIdx, name:'수정 데이터', englishName:'Update Data'})
             .then((result) => {
                 expect(result).eq(1);
                 done();
@@ -92,19 +92,19 @@ describe('# ingredientDao Test', () => {
             });
         });
         after(async () => {
-            await pool.queryParam_None(`DELETE FROM ingredient WHERE ingredient_idx=${ingredient_idx}`);
+            await pool.queryParam_None(`DELETE FROM ingredient WHERE ingredient_idx=${ingredientIdx}`);
         });
     });
     
     describe('# delete Test', () => {
-        let ingredient_idx;
+        let ingredientIdx;
         before(async () => {
             await pool.queryParam_None("DELETE FROM ingredient WHERE name='삭제 데이터'");
             const result = await pool.queryParam_None("INSERT ingredient(name, english_name) values('삭제 데이터','Delete Data')");
-            ingredient_idx = result.insertId;
+            ingredientIdx = result.insertId;
         });
         it('# success case', (done) => {
-            ingredientDao.delete(ingredient_idx).then((result) => {
+            ingredientDao.delete(ingredientIdx).then((result) => {
                 expect(result).eq(1);
                 done();
             }).catch((err) => {
@@ -113,8 +113,8 @@ describe('# ingredientDao Test', () => {
             });
         });
         after(async () => {
-            if(!ingredient_idx) return;
-            await pool.queryParam_None(`DELETE FROM ingredient WHERE ingredient_idx=${ingredient_idx}`);
+            if(!ingredientIdx) return;
+            await pool.queryParam_None(`DELETE FROM ingredient WHERE ingredient_idx=${ingredientIdx}`);
         });
     });
 })
