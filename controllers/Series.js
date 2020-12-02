4da9fc66f57785f2cb9546d0e6046715239a8f0c
yejin <jinee0717@daum.net>
2020-12-02 21:46:02 +0900

    [CONTROLLER, SERVICE] series contoller, service 구현

--- controllers/Series.js
+++ controllers/Series.js
@@ -1,13 +1,16 @@
 'use strict';
 
-var utils = require('../utils/writer.js');
-var Series = require('../service/SeriesService');
+const utils = require('../utils/writer.js');
+const Series = require('../service/SeriesService');
 
-module.exports.deleteSeries = function deleteSeries (req, res, next) {
-  var seriesIdx = req.swagger.params['seriesIdx'].value;
-  Series.deleteSeries(seriesIdx)
+module.exports.postSeries = function postSeries (req, res, next) {
+  const {name, englishName, description} = req.swagger.params['body'].value;
+  Series.postSeries({name, englishName, description})
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, utils.respondWithCode(200, {
+        message: 'series post 성공',
+        data: response
+      }));
     })
     .catch(function (response) {
       utils.writeJson(res, response);
@@ -15,10 +18,13 @@ module.exports.deleteSeries = function deleteSeries (req, res, next) {
 };
 
 module.exports.getSeriesByIdx = function getSeriesByIdx (req, res, next) {
-  var seriesIdx = req.swagger.params['seriesIdx'].value;
+  const seriesIdx = req.swagger.params['seriesIdx'].value;
   Series.getSeriesByIdx(seriesIdx)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, utils.respondWithCode(200, {
+        message: 'series 개별 조회 성공',
+        data: response
+      }));
     })
     .catch(function (response) {
       utils.writeJson(res, response);
@@ -28,18 +34,11 @@ module.exports.getSeriesByIdx = function getSeriesByIdx (req, res, next) {
 module.exports.getSeriesList = function getSeriesList (req, res, next) {
   Series.getSeriesList()
     .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
-};
-
-module.exports.postSeries = function postSeries (req, res, next) {
-  var body = req.swagger.params['body'].value;
-  Series.postSeries(body)
-    .then(function (response) {
-      utils.writeJson(res, response);
+      console.log("controller -> series getList");
+      utils.writeJson(res, utils.respondWithCode(200, {
+        message: 'series 전체 조회 성공',
+        data: response
+      }));
     })
     .catch(function (response) {
       utils.writeJson(res, response);
@@ -47,11 +46,26 @@ module.exports.postSeries = function postSeries (req, res, next) {
 };
 
 module.exports.putSeries = function putSeries (req, res, next) {
-  var seriesIdx = req.swagger.params['seriesIdx'].value;
-  var body = req.swagger.params['body'].value;
-  Series.putSeries(seriesIdx,body)
+  const seriesIdx = req.swagger.params['seriesIdx'].value;
+  const {name, englishName, description} = req.swagger.params['body'].value;
+  Series.putSeries({seriesIdx, name, englishName, description})
     .then(function (response) {
+      utils.writeJson(res, utils.respondWithCode(200, {
+        message: 'series put 성공'
+      }));
+    })
+    .catch(function (response) {
       utils.writeJson(res, response);
+    });
+};
+
+module.exports.deleteSeries = function deleteSeries (req, res, next) {
+  const seriesIdx = req.swagger.params['seriesIdx'].value;
+  Series.deleteSeries(seriesIdx)
+    .then(function (response) {
+      utils.writeJson(res, utils.respondWithCode(200, {
+        message: 'series delete 성공'
+      }));
     })
     .catch(function (response) {
       utils.writeJson(res, response);
--- service/SeriesService.js
+++ service/SeriesService.js
@@ -1,73 +1,6 @@
 'use strict';
 
-
-/**
- * 계열 삭제
- * 계열 삭제
- *
- * seriesIdx Long 계열 ID
- * no response value expected for this operation
- **/
-exports.deleteSeries = function(seriesIdx) {
-  return new Promise(function(resolve, reject) {
-    resolve();
-  });
-}
-
-
-/**
- * 특정 계열 조회
- * 특정 계열 조회
- *
- * seriesIdx Long 계열 ID
- * returns SeriesInfo
- **/
-exports.getSeriesByIdx = function(seriesIdx) {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = {
-  "name" : "꿀",
-  "englishName" : "Honey",
-  "description" : "화이트 허니, 허니",
-  "seriesIdx" : 1
-};
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
-  });
-}
-
-
-/**
- * 계열 전체 목록 조회
- * 계열 전체 목록 반환
- *
- * returns List
- **/
-exports.getSeriesList = function() {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "name" : "꿀",
-  "englishName" : "Honey",
-  "description" : "화이트 허니, 허니",
-  "seriesIdx" : 1
-}, {
-  "name" : "꿀",
-  "englishName" : "Honey",
-  "description" : "화이트 허니, 허니",
-  "seriesIdx" : 1
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
-  });
-}
-
+const seriesDAO = require('../dao/SeriesDao.js');
 
 /**
  * 계열 삽입
@@ -76,11 +9,33 @@ exports.getSeriesList = function() {
  * body SeriesInfo Insert new series info (optional)
  * no response value expected for this operation
  **/
-exports.postSeries = function(body) {
-  return new Promise(function(resolve, reject) {
-    resolve();
-  });
+exports.postSeries = ({name, englishName, description}) => {
+  return seriesDAO.create({name, englishName, description});
 }
+
+/**
+ * 특정 계열 조회
+ * 특정 계열 조회
+ *
+ * seriesIdx Long 계열 ID
+ * returns SeriesInfo
+ **/
+exports.getSeriesByIdx = (seriesIdx) => {
+  return seriesDAO.read(seriesIdx);
+};
+
+
+/**
+ * 계열 전체 목록 조회
+ * 계열 전체 목록 반환
+ *
+ * returns List
+ **/
+exports.getSeriesList = () => {
+  console.log("service -> series getList");
+  return seriesDAO.readAll();
+};
+
 
 
 /**
@@ -91,9 +46,18 @@ exports.postSeries = function(body) {
  * body SeriesInfo Updated series info (optional)
  * no response value expected for this operation
  **/
-exports.putSeries = function(seriesIdx,body) {
-  return new Promise(function(resolve, reject) {
-    resolve();
-  });
-}
+exports.putSeries = ({seriesIdx, name, englishName, description}) => {
+  return seriesDAO.update({seriesIdx, name, englishName, description});
+};
 
+
+/**
+ * 계열 삭제
+ * 계열 삭제
+ *
+ * seriesIdx Long 계열 ID
+ * no response value expected for this operation
+ **/
+exports.deleteSeries = (seriesIdx) => {
+  return seriesDAO.delete(seriesIdx);
+}
