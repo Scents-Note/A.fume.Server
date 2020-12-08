809acf01c64ea8d71ec645a40f4e1edf715071af
윤희성 <heesung6701@naver.com>
2020-12-08 23:36:26 +0900

    [perfumeDao] 성별 나이에 맞는 향수 추천
    recommendPerfumeByAgeAndGender 구현

--- dao/PerfumeDao.js
+++ dao/PerfumeDao.js
@@ -50,9 +50,30 @@ const SQL_RECENT_SEARCH_PERFUME_SELECT = 'SELECT ' +
 'GROUP BY sh.perfume_idx ' +
 'ORDER BY createTime DESC ' +
 'LIMIT 10 ';
+const SQL_RECOMMEND_PERFUME_BY_AGE_AND_GENDER__SELECT = 'SELECT ' +
+'COUNT(*) as weight, ' +
+'p.perfume_idx as perfumeIdx, p.main_series_idx as mainSeriesIdx, p.brand_idx as brandIdx, p.name, p.english_name as englishName, p.image_thumbnail_url as imageUrl, p.release_date as releaseDate, ' +
+'b.name as brandName, ' +
+'s.name as mainSeriesName, ' +
+'(SELECT COUNT(*) FROM like_perfume lp WHERE lp.perfume_idx = p.perfume_idx) as likeCnt, ' +
+'(SELECT COUNT(*) FROM like_perfume lp WHERE lp.perfume_idx = p.perfume_idx AND lp.user_idx = ?) as isLiked ' +
+'FROM search_history sh ' +
+'INNER JOIN perfume p ON sh.perfume_idx = p.perfume_idx ' +
+'INNER JOIN brand b ON p.brand_idx = b.brand_idx ' +
+'INNER JOIN series s ON p.main_series_idx = s.series_idx ' +
+'INNER JOIN user u ON sh.user_idx = u.user_idx ' +
+'WHERE u.gender = ? AND (u.birth BETWEEN ? AND ?) ' +
+'GROUP BY sh.perfume_idx ' +
+'ORDER BY weight DESC ' +
+'LIMIT 10 ';
 const SQL_PERFUME_UPDATE = 'UPDATE perfume SET brand_idx = ?, main_series_idx = ?, name = ?, english_name = ?, image_thumbnail_url = ?, release_date = ? WHERE perfume_idx = ?';
 const SQL_PERFUME_DETAIL_UPDATE = 'UPDATE perfume_detail SET story = ?, abundance_rate = ?, volume_and_price = ?, image_url = ? WHERE perfume_idx = ?';
 const SQL_PERFUME_DELETE = 'DELETE FROM perfume WHERE perfume_idx = ?';
+
+const genderMap = {
+    '남자': 1,
+    '여자': 2,
+};
 
 /**
  * 향수 추가
@@ -166,6 +187,24 @@ module.exports.recentSearchPerfumeList = async (userIdx) => {
     const result = await pool.queryParam_Parse(SQL_RECENT_SEARCH_PERFUME_SELECT, [userIdx, userIdx]);
     return result.map(it => {
         delete it.createTime
+        return it;
+    });
+};
+
+/**
+ * 나이 및 성별에 기반한 향수 추천
+ * 
+ * @param {string} gender
+ * @param {number} startBirth
+ * @param {number} endBirth
+ * @returns {Promise<Perfume[]>}
+ */
+module.exports.recommendPerfumeByAgeAndGender = async (userIdx, gender, startBirth, endBirth) => {
+    gender = genderMap[gender] || 2;
+    const result = await pool.queryParam_Parse(SQL_RECOMMEND_PERFUME_BY_AGE_AND_GENDER__SELECT,  [userIdx, gender, startBirth, endBirth]);
+    return result.map(it => {
+        delete it.createTime
+        delete it.weight
         return it;
     });
 };
