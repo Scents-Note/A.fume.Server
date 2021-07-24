2f729d481ea88667a1cd44debc08003d77f4fd3a
윤희성 <heesung6701@naver.com>
2021-07-24 15:15:25 +0900

    Modify BrandDao.spec.js

--- test/dao/BrandDao.spec.js
+++ test/dao/BrandDao.spec.js
@@ -16,7 +16,26 @@ const { PagingVO } = require('../../data/vo');
 BrandDTO.prototype.validTest = function () {
     expect(this.brandIdx).to.be.ok;
     expect(this.name).to.be.ok;
-    expect(this.firstInitial).to.be.ok;
+    expect(this.firstInitial).to.be.oneOf([
+        'ㄱ',
+        'ㄲ',
+        'ㄴ',
+        'ㄷ',
+        'ㄸ',
+        'ㄹ',
+        'ㅁ',
+        'ㅂ',
+        'ㅃ',
+        'ㅅ',
+        'ㅆ',
+        'ㅇ',
+        'ㅈ',
+        'ㅉ',
+        'ㅊ',
+        'ㅋ',
+        'ㅍ',
+        'ㅎ',
+    ]);
     expect(this.imageUrl).to.be.ok;
     expect(this.description).to.be.not.undefined;
     expect(this.createdAt).to.be.ok;
