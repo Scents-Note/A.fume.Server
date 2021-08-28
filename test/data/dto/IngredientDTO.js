bbf6f437e44ac1e83457fce2edbd2b8fa5b84c7e
Heesung Youn <heesung6701@naver.com>
2021-08-28 23:10:54 +0900

    Add IngredientDTO for Test

--- test/data/dto/IngredientDTO.js
+++ test/data/dto/IngredientDTO.js
@@ -5,8 +5,9 @@ const IngredientDTO = require('../../../data/dto/IngredientDTO');
 IngredientDTO.prototype.validTest = function () {
     expect(this.ingredientIdx).to.be.ok;
     expect(this.name).to.be.ok;
+    expect(this.englishName).to.be.ok;
+    expect(this.description).to.be.ok;
     expect(this.imageUrl).to.be.ok;
-    expect(this.description).to.be.not.undefined;
     expect(this.seriesIdx).to.be.ok;
     expect(this.createdAt).to.be.ok;
     expect(this.updatedAt).to.be.ok;
