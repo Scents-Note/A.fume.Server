9e3928fea588a1bb9abc2f66fe88f12f77992525
Heesung Youn <heesung6701@naver.com>
2021-08-28 22:23:51 +0900

    update code

--- utils/code.js
+++ utils/code.js
@@ -1,7 +1,7 @@
 module.exports = {
     GENDER_MAN: 1,
     GENDER_WOMAN: 2,
-    abundanceRateArr: [
+    ABUNDANCE_RATE_LIST: [
         'None',
         '오 드 코롱',
         '코롱',
@@ -9,4 +9,20 @@ module.exports = {
         '오 드 퍼퓸',
         '퍼퓸',
     ],
+    NOTE_TYPE_TOP: 1,
+    NOTE_TYPE_MIDDLE: 2,
+    NOTE_TYPE_BASE: 3,
+    NOTE_TYPE_SINGLE: 4,
+    SEASONAL_LIST: ['None', 'spring', 'summer', 'fall', 'winter'],
+    SILLAGE_LIST: ['None', 'light', 'normal', 'heavy'],
+    LONGEVITY_LIST: [
+        'None',
+        'veryWeak',
+        'weak',
+        'medium',
+        'strong',
+        'veryStrong',
+    ],
+    GENDER_LIST: ['None', 'male', 'neutral', 'female'],
+    NOTE_TYPE_LIST: ['None', 'top', 'middle', 'base', 'single'],
 };
