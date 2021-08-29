1e70a26c7c92f36648a39f7c2423d011d4c60187
윤희성 <heesung6701@naver.com>
2021-08-29 17:40:30 +0900

    Implement IngredientService

--- service/IngredientService.js
+++ service/IngredientService.js
@@ -1,45 +1,11 @@
 'use strict';
 
 let ingredientDao = require('../dao/IngredientDao.js');
-let seriesDao = require('../dao/SeriesDao.js');
-const { parseSortToOrder } = require('../utils/parser.js');
-const { IngredientDTO, ListAndCountDTO } = require('../data/dto');
-/**
- * 향료 삽입
- *
- * @param {Object} ingredient
- * @return {Promise<number>}
- **/
-exports.postIngredient = ({
-    name,
-    englishName,
-    description,
-    imageUrl,
-    seriesIdx,
-}) => {
-    return ingredientDao.create({
-        name,
-        englishName,
-        description,
-        imageUrl,
-        seriesIdx,
-    });
-};
-
-/**
- * 특정 향료 조회
- *
- * @param {number} ingredientIdx
- * @returns {Promise<Ingredient>}
- **/
-exports.getIngredientByIdx = (ingredientIdx) => {
-    return ingredientDao.readByIdx(ingredientIdx);
-};
 
 /**
  * 향료 목록 조회
  *
- * @returns {Promise<Ingredient[]>}
+ * @returns {Promise<ListAndCountDTO<IngredientDTO>>} ListAndCountDTO<IngredientDTO>
  **/
 exports.getIngredientAll = () => {
     return ingredientDao.readAll();
@@ -48,76 +14,8 @@ exports.getIngredientAll = () => {
 /**
  * 재료 검색
  *
- * @param {number} pagingIndex
- * @param {number} pagingSize
- * @param {string} sort
- * @returns {Promise<Ingredient[]>}
- **/
-exports.searchIngredient = (pagingIndex, pagingSize, sort) => {
-    const order = parseSortToOrder(sort);
-    return ingredientDao.search(pagingIndex, pagingSize, order);
-};
-
-/**
- * 향료 수정
- *
- * @param {Object} Ingredient
- * @returns {Promise<number>} affectedRows
- **/
-exports.putIngredient = ({
-    ingredientIdx,
-    name,
-    englishName,
-    imageUrl,
-    description,
-}) => {
-    return ingredientDao.update({
-        ingredientIdx,
-        name,
-        englishName,
-        imageUrl,
-        description,
-    });
-};
-
-/**
- * 향료 수정
- *
- * @param {Object} Ingredient
- * @returns {Promise<number>} affectedRows
- **/
-exports.patchIngredient = (ingredientIdx, obj) => {
-    return ingredientDao.update(Object.assign({ ingredientIdx }, obj));
-};
-
-/**
- * 향료 삭제
- *
- * @param {number} ingredientIdx
- * @returns {Promise<number>}
- **/
-exports.deleteIngredient = (ingredientIdx) => {
-    return ingredientDao.delete(ingredientIdx);
-};
-
-/**
- * 재료에 해당하는 계열 조회
- *
- * @param {number} ingredientIdx
- * @returns {Promise<Series[]>}
- */
-exports.getSeriesList = (ingredientIdx) => {
-    return seriesDao.readByIngredientIdx(ingredientIdx).then((it) => {
-        delete it.JoinSeriesIngredient;
-        return it;
-    });
-};
-
-/**
- * 재료 검색
- *
  * @param {IngredientConditionDTO} ingredientConditionDTO
- * @returns {Promise<Ingredient>}
+ * @returns {Promise<IngredientDTTO>} ingredientDTO
  **/
 exports.findIngredient = (ingredientConditionDTO) => {
     return ingredientDao.findIngredient(ingredientConditionDTO);
@@ -127,16 +25,12 @@ exports.findIngredient = (ingredientConditionDTO) => {
  * 계열에 해당하는 재료 조회
  *
  * @param {number} seriesIdx
- * @returns {Promise<Ingredient[]>}
+ * @returns {Promise<ListAndCountDTO<IngredientDTO>>} ListAndCountDTO<IngredientDTO>
  */
 exports.getIngredientList = (seriesIdx) => {
-    return ingredientDao.readAll({ seriesIdx }, { nest: true, raw: true });
+    return ingredientDao.readAll({ seriesIdx });
 };
 
 module.exports.setIngredientDao = (dao) => {
     ingredientDao = dao;
-};
-
-module.exports.setSeriesDao = (dao) => {
-    seriesDao = dao;
 };
--- test/service/IngredientService.spec.js
+++ test/service/IngredientService.spec.js
@@ -5,27 +5,60 @@ const chai = require('chai');
 const { expect } = chai;
 
 const IngredientDTO = require('../data/dto/IngredientDTO');
-
-const ingredientService = require('../../service/IngredientService');
-ingredientService.setIngredientDao(require('../dao/IngredientDao.mock.js'));
-ingredientService.setSeriesDao(require('../dao/SeriesDao.mock.js'));
-
 const ListAndCountDTO = require('../data/dto/ListAndCountDTO');
 
+const ingredientService = require('../../service/IngredientService');
+const mockIngredientDAO = Object.assign(
+    {},
+    require('../dao/IngredientDao.mock.js')
+);
+ingredientService.setIngredientDao(mockIngredientDAO);
+
 describe('# Ingredient Service Test', () => {
-    describe('# getIngredientList Test', () => {
-        it('# success Test', (done) => {
-            ingredientService
-                .getIngredientList(1)
-                .then((result) => {
-                    expect(result).instanceOf(ListAndCountDTO);
-                    result.validTest((item) => {
-                        expect(item).instanceOf(IngredientDTO);
-                        item.validTest();
-                    });
-                    done();
-                })
-                .catch((err) => done(err));
+    describe('# read test', () => {
+        describe('# findSIngredient Test', () => {
+            it('# success Test', (done) => {
+                ingredientService
+                    .findIngredient({ name: '재료 이름' })
+                    .then((result) => {
+                        expect(result).instanceOf(IngredientDTO);
+                        result.validTest();
+                        done();
+                    })
+                    .catch((err) => done(err));
+            });
+        });
+
+        describe('# getIngredientAll Test', () => {
+            it('# success Test', (done) => {
+                ingredientService
+                    .getIngredientAll()
+                    .then((result) => {
+                        expect(result).instanceOf(ListAndCountDTO);
+                        result.validTest((item) => {
+                            expect(item).instanceOf(IngredientDTO);
+                            item.validTest();
+                        });
+                        done();
+                    })
+                    .catch((err) => done(err));
+            });
+        });
+
+        describe('# getIngredientList Test', () => {
+            it('# success Test', (done) => {
+                ingredientService
+                    .getIngredientList(1)
+                    .then((result) => {
+                        expect(result).instanceOf(ListAndCountDTO);
+                        result.validTest((item) => {
+                            expect(item).instanceOf(IngredientDTO);
+                            expect(item.seriesIdx).to.be.eq(1);
+                        });
+                        done();
+                    })
+                    .catch((err) => done(err));
+            });
         });
     });
 });
