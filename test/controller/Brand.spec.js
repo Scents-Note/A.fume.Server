0d7ed1d441985b65946a89e32def7e805313fa98
Heesung Youn <heesung6701@naver.com>
2021-09-16 22:34:30 +0900

    remove unused code

--- test/controller/Brand.spec.js
+++ test/controller/Brand.spec.js
@@ -56,26 +56,6 @@ Brand.setBrandService({
 });
 
 describe('# Brand Controller Test', () => {
-    // describe('# searchBrand Test', () => {
-    //     it('success case', (done) => {
-    //         request(app)
-    //             .get(`${basePath}/brand/search`)
-    //             .expect((res) => {
-    //                 expect(res.status).to.be.eq(200);
-    //                 const { message, data } = res.body;
-    //                 expect(message).to.be.eq('브랜드 검색 성공');
-    //                 expect(data.count).to.be.gt(0);
-    //                 for (const brand of data.rows) {
-    //                     expect(brand).to.be.have.property('brandIdx');
-    //                     expect(brand).to.be.have.property('name');
-    //                     expect(Object.entries(brand).length).to.be.eq(2);
-    //                 }
-    //                 done();
-    //             })
-    //             .catch((err) => done(err));
-    //     });
-    // });
-
     describe('# getBrandAll Test', () => {
         it('success case', (done) => {
             request(app)
@@ -96,92 +76,6 @@ describe('# Brand Controller Test', () => {
                 .catch((err) => done(err));
         });
     });
-
-    // describe('# postBrand Test', () => {
-    //     it('success case', (done) => {
-    //         request(app)
-    //             .post(`${basePath}/brand`)
-    //             .set('x-access-token', 'Bearer {token}')
-    //             .send(mockBrandInputDTO)
-    //             .expect((res) => {
-    //                 expect(res.status).to.be.eq(200);
-    //                 const { message, data } = res.body;
-    //                 expect(message).to.be.eq('브랜드 추가 성공');
-    //                 expect(data).to.be.eq(1);
-    //                 done();
-    //             })
-    //             .catch((err) => done(err));
-    //     });
-
-    //     it('No permission case', (done) => {
-    //         request(app)
-    //             .post(`${basePath}/brand`)
-    //             .send(mockBrandInputDTO)
-    //             .expect((res) => {
-    //                 expect(res.status).to.be.eq(401);
-    //                 const { message } = res.body;
-    //                 expect(message).to.be.eq('권한이 없습니다.');
-    //                 done();
-    //             })
-    //             .catch((err) => done(err));
-    //     });
-    // });
-
-    // describe('# putBrand Test', () => {
-    //     it('success case', (done) => {
-    //         request(app)
-    //             .put(`${basePath}/brand/1`)
-    //             .set('x-access-token', 'Bearer {token}')
-    //             .send(mockBrandInputDTO)
-    //             .expect((res) => {
-    //                 expect(res.status).to.be.eq(200);
-    //                 const { message } = res.body;
-    //                 expect(message).to.be.eq('브랜드 수정 성공');
-    //                 done();
-    //             })
-    //             .catch((err) => done(err));
-    //     });
-
-    //     it('No permission case', (done) => {
-    //         request(app)
-    //             .put(`${basePath}/brand/1`)
-    //             .send(mockBrandInputDTO)
-    //             .expect((res) => {
-    //                 expect(res.status).to.be.eq(401);
-    //                 const { message } = res.body;
-    //                 expect(message).to.be.eq('권한이 없습니다.');
-    //                 done();
-    //             })
-    //             .catch((err) => done(err));
-    //     });
-    // });
-
-    // describe('# deleteBrand Test', () => {
-    //     it('success case', (done) => {
-    //         request(app)
-    //             .delete(`${basePath}/brand/1`)
-    //             .set('x-access-token', 'Bearer {token}')
-    //             .expect((res) => {
-    //                 expect(res.status).to.be.eq(200);
-    //                 const { message } = res.body;
-    //                 expect(message).to.be.eq('브랜드 삭제 성공');
-    //                 done();
-    //             })
-    //             .catch((err) => done(err));
-    //     });
-
-    //     it('No permission case', (done) => {
-    //         request(app)
-    //             .delete(`${basePath}/brand/1`)
-    //             .expect((res) => {
-    //                 expect(res.status).to.be.eq(401);
-    //                 const { message } = res.body;
-    //                 expect(message).to.be.eq('권한이 없습니다.');
-    //                 done();
-    //             })
-    //             .catch((err) => done(err));
-    //     });
-    // });
 
     describe('# getFilterBrand Test', () => {
         it('success case', (done) => {
