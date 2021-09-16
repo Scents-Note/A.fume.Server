c8216af5022d635165061ab338b732efb767d4dd
Heesung Youn <heesung6701@naver.com>
2021-09-16 22:44:10 +0900

    BrandDTO mock 객체 만드는거 BrandDTO로 위임

--- test/data/dto/BrandDTO.js
+++ test/data/dto/BrandDTO.js
@@ -34,4 +34,22 @@ BrandDTO.prototype.validTest = function () {
     expect(this.updatedAt).to.be.ok;
 };
 
+BrandDTO.create = function (condition) {
+    return new BrandDTO(
+        Object.assign(
+            {
+                brandIdx: 1,
+                name: '브랜드1',
+                englishName: 'BRAND1',
+                firstInitial: 'ㅂ',
+                imageUrl: 'http://',
+                description: '이것은 브랜드',
+                createdAt: '2021-07-24T03:38:52.000Z',
+                updatedAt: '2021-07-24T03:38:52.000Z',
+            },
+            condition
+        )
+    );
+};
+
 module.exports = BrandDTO;
