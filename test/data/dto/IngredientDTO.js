b1f4ab34b78bde75658444db9d1c29dca18d02b8
Heesung Youn <heesung6701@naver.com>
2021-09-17 00:03:13 +0900

    IngredientMock 객체 생성 Model로 위임

--- test/data/dto/IngredientDTO.js
+++ test/data/dto/IngredientDTO.js
@@ -12,4 +12,17 @@ IngredientDTO.prototype.validTest = function () {
     expect(this.updatedAt).to.be.ok;
 };
 
+IngredientDTO.createWithIdx = function ({ ingredientIdx, seriesIdx }) {
+    return new IngredientDTO({
+        ingredientIdx,
+        name: `재료${ingredientIdx}`,
+        englishName: `Ingredient ${ingredientIdx}`,
+        description: `이것은 재료 ${ingredientIdx}`,
+        imageUrl: 'http://',
+        seriesIdx,
+        createdAt: '2021-07-24T03:38:52.000Z',
+        updatedAt: '2021-07-24T03:38:52.000Z',
+    });
+};
+
 module.exports = IngredientDTO;
