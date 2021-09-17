63457023b47439308e897ca755a682f7e63fc12c
Heesung Youn <heesung6701@naver.com>
2021-09-17 21:53:03 +0900

    Add IngredientResponseDTO valid code

--- test/data/response_dto/series/SeriesFilterResponseDTO.js
+++ test/data/response_dto/series/SeriesFilterResponseDTO.js
@@ -2,8 +2,15 @@ const expect = require('../../../utils/expect');
 
 const SeriesFilterResponseDTO = require('../../../data/response_dto/series/SeriesResponseDTO');
 
+const IngredientResponseDTO = require('../ingredient/IngredientResponseDTO');
+
 SeriesFilterResponseDTO.validTest = function () {
     expect.hasProperties.call(this, 'seriesIdx', 'name', 'ingredients');
+    expect(this.seriesIdx).to.be.ok;
+    expect(this.name).to.be.ok;
+    this.ingredients.forEach((item) => {
+        IngredientResponseDTO.validTest.call(item);
+    });
 };
 
 module.exports = SeriesFilterResponseDTO;
