4f7423c7f2f629b60e78d80723168f54eec76d79
윤희성 <heesung6701@naver.com>
2021-09-26 17:54:54 +0900

    createWithIdx 로 mock객체 생성

--- test/dao/IngredientDao.mock.js
+++ test/dao/IngredientDao.mock.js
@@ -2,41 +2,19 @@ const ListAndCountDTO = require('../data/dto/ListAndCountDTO');
 const IngredientDTO = require('../data/dto/IngredientDTO');
 
 module.exports.readByIdx = async (ingredientIdx) => {
-    return new IngredientDTO({
-        ingredientIdx,
-        seriesIdx: 4,
-        name: '재료 이름',
-        englishName: 'ingredient english name',
-        description: 'ingredient description',
-        imageUrl: 'https://www.naver.com',
-        createdAt: '2021-07-13T11:33:49.000Z',
-        updatedAt: '2021-08-07T09:20:29.000Z',
-    });
+    return IngredientDTO.createWithIdx({ ingredientIdx, seriesIdx: 1 });
 };
 
 module.exports.readByName = async (ingredientName) => {
     return IngredientDTO.create({ ingredientName });
 };
 
-module.exports.readAll = async (where) => {
+module.exports.readAll = async (where = {}) => {
+    const seriesIdx = where.seriesIdx || 1;
     return new ListAndCountDTO({
         count: 5,
         rows: [1, 2, 3, 4, 5].map((idx) =>
-            IngredientDTO.create(
-                Object.assign(
-                    {
-                        ingredientIdx: idx,
-                        seriesIdx: idx,
-                        name: `재료 ${idx}`,
-                        englishName: `ingredient english name ${idx}`,
-                        description: `ingredient description ${idx}`,
-                        imageUrl: `https://www.naver.com/${idx}`,
-                        createdAt: '2021-07-13T11:33:49.000Z',
-                        updatedAt: '2021-08-07T09:20:29.000Z',
-                    },
-                    where
-                )
-            )
+            IngredientDTO.createWithIdx({ ingredientIdx: idx, seriesIdx })
         ),
     });
 };
@@ -44,35 +22,15 @@ module.exports.readAll = async (where) => {
 module.exports.search = async (pagingIndex, pagingSize, order) => {
     return new ListAndCountDTO({
         count: 5,
-        rows: [1, 2, 3, 4, 5].map(
-            (idx) =>
-                new IngredientDTO({
-                    ingredientIdx: idx,
-                    seriesIdx: idx,
-                    name: `재료 ${idx}`,
-                    englishName: `ingredient english name ${idx}`,
-                    description: `ingredient description ${idx}`,
-                    imageUrl: `https://www.naver.com/${idx}`,
-                    createdAt: '2021-07-13T11:33:49.000Z',
-                    updatedAt: '2021-08-07T09:20:29.000Z',
-                })
+        rows: [1, 2, 3, 4, 5].map((idx) =>
+            IngredientDTO.createWithIdx({ ingredientIdx: idx, seriesIdx: idx })
         ),
     });
 };
 
 module.exports.readBySeriesIdxList = async (seriesIdxList) => {
-    return [1, 2, 3, 4, 5].map(
-        (idx) =>
-            new IngredientDTO({
-                ingredientIdx: idx,
-                seriesIdx: idx,
-                name: `재료 ${idx}`,
-                englishName: `ingredient english name ${idx}`,
-                description: `ingredient description ${idx}`,
-                imageUrl: `https://www.naver.com/${idx}`,
-                createdAt: '2021-07-13T11:33:49.000Z',
-                updatedAt: '2021-08-07T09:20:29.000Z',
-            })
+    return [1, 2, 3, 4, 5].map((idx) =>
+        IngredientDTO.createWithIdx({ ingredientIdx: idx, seriesIdx: idx })
     );
 };
 
