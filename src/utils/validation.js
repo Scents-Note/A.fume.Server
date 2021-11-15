6286961d65f9ddeffd5b1f53436643b6e0d3cde1
Heesung Youn <heesung6701@naver.com>
2021-11-16 00:11:13 +0900

    remove validation

--- src/utils/validation.js
+++
@@ -1,9 +0,0 @@
-import { InvalidRequestError } from './errors/errors';
-
-module.exports.validateType = (obj, key, type, next) => {
-    if (obj[key] instanceof type) {
-        return true;
-    }
-    next(new InvalidRequestError(key, type, obj[key]));
-    return false;
-};
