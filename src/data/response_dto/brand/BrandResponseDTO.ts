6aefa8a1b71cc77c7d4da3409d49a04b0cbf2e2d
윤희성 <heesung6701@naver.com>
2021-11-27 21:40:16 +0900

    Brand Controller에 RequestHandler 적용

--- src/data/response_dto/brand/BrandResponseDTO.ts
+++ src/data/response_dto/brand/BrandResponseDTO.ts
@@ -1,9 +1,14 @@
+import BrandDTO from '../../dto/BrandDTO';
+
 class BrandResponseDTO {
     brandIdx: number;
     name: string;
     constructor(brandIdx: number, name: string) {
         this.brandIdx = brandIdx;
         this.name = name;
+    }
+    static create(brandDTO: BrandDTO): BrandResponseDTO {
+        return new BrandResponseDTO(brandDTO.brandIdx, brandDTO.name);
     }
 }
 
