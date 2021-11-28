f47fd03f573638a96aad63ebf47e60da6caef7d0
윤희성 <heesung6701@naver.com>
2021-11-28 09:37:00 +0900

    add UserInputDTO(nullable UserDTO)

---
+++ src/data/dto/UserInputDTO.ts
@@ -0,0 +1,46 @@
+import { Gender, Grade } from '../../utils/enumType';
+
+class UserInputDTO {
+    userIdx?: number;
+    nickname?: string;
+    password?: string;
+    gender?: Gender;
+    email?: string;
+    birth?: number;
+    grade?: Grade;
+    accessTime?: string;
+    constructor(
+        userIdx?: number,
+        nickname?: string,
+        password?: string,
+        gender?: Gender,
+        email?: string,
+        birth?: number,
+        grade?: Grade,
+        accessTime?: string
+    ) {
+        this.userIdx = userIdx;
+        this.nickname = nickname;
+        this.password = password;
+        this.gender = gender;
+        this.email = email;
+        this.birth = birth;
+        this.grade = grade;
+        this.accessTime = accessTime;
+    }
+
+    static createByJson(json: any): UserInputDTO {
+        return new UserInputDTO(
+            json.userIdx,
+            json.nickname,
+            json.password,
+            json.gender,
+            json.email,
+            json.birth,
+            json.grade,
+            json.accessTime
+        );
+    }
+}
+
+export default UserInputDTO;
