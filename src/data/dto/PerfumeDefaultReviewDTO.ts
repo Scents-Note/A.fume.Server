796414164cab66d250c36017391c42445a58ee08
Heesung Youn <heesung6701@naver.com>
2022-01-17 22:55:31 +0900

    map(parseInt)하면 NaN 나오는 현상 수정.
    parseInt의 두번째 인자에 의도하지 않는 값이 전달되면서 NaN이 나타난다.

--- src/data/dto/PerfumeDefaultReviewDTO.ts
+++ src/data/dto/PerfumeDefaultReviewDTO.ts
@@ -65,10 +65,18 @@ class PerfumeDefaultReviewDTO {
             sillage,
             longevity,
         }: { [key: string]: string } = json;
-        const seasonalList: number[] = seasonal.split('/').map(parseInt);
-        const sillageList: number[] = sillage.split('/').map(parseInt);
-        const longevityList: number[] = longevity.split('/').map(parseInt);
-        const genderList: number[] = gender.split('/').map(parseInt);
+        const seasonalList: number[] = seasonal
+            .split('/')
+            .map((it) => parseInt(it));
+        const sillageList: number[] = sillage
+            .split('/')
+            .map((it) => parseInt(it));
+        const longevityList: number[] = longevity
+            .split('/')
+            .map((it) => parseInt(it));
+        const genderList: number[] = gender
+            .split('/')
+            .map((it) => parseInt(it));
         return new PerfumeDefaultReviewDTO(
             json.perfumeIdx,
             json.rating,
