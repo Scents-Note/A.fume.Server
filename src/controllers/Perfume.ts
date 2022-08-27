eff66d021cacf655ce4f9692a8525e1c5566fa18
hee.youn <heesung6701@naver.com>
2022-08-27 17:33:49 +0900

    로깅을 위한 JSON.stringify 추가

--- src/controllers/Perfume.ts
+++ src/controllers/Perfume.ts
@@ -99,7 +99,9 @@ const getPerfume: RequestHandler = (
     }
     const loginUserIdx: number = req.middlewareToken.loginUserIdx || -1;
     logger.debug(
-        `${LOG_TAG} likePerfume(userIdx = ${loginUserIdx}, params = ${req.params})`
+        `${LOG_TAG} likePerfume(userIdx = ${loginUserIdx}, params = ${JSON.stringify(
+            req.params
+        )})`
     );
     Promise.all([
         Perfume.getPerfumeById(perfumeIdx, loginUserIdx),
@@ -196,7 +198,9 @@ const searchPerfume: RequestHandler = (
         req.query
     );
     logger.debug(
-        `${LOG_TAG} likePerfume(userIdx = ${loginUserIdx}, query = ${req.query}, body = ${req.body})`
+        `${LOG_TAG} likePerfume(userIdx = ${loginUserIdx}, query = ${JSON.stringify(
+            req.query
+        )}, body = ${JSON.stringify(req.body)})`
     );
     const perfumeSearchDTO: PerfumeSearchDTO =
         perfumeSearchRequest.toPerfumeSearchDTO(loginUserIdx);
@@ -267,7 +271,9 @@ const likePerfume: RequestHandler = (
     const perfumeIdx: number = req.params['perfumeIdx'];
     const loginUserIdx: number = req.middlewareToken.loginUserIdx;
     logger.debug(
-        `${LOG_TAG} likePerfume(userIdx = ${loginUserIdx}, params = ${req.params})`
+        `${LOG_TAG} likePerfume(userIdx = ${loginUserIdx}, params = ${JSON.stringify(
+            req.params
+        )})`
     );
     Perfume.likePerfume(loginUserIdx, perfumeIdx)
         .then((result: boolean) => {
@@ -345,7 +351,9 @@ const getRecentPerfume: RequestHandler = (
         req.query
     );
     logger.debug(
-        `${LOG_TAG} getRecentPerfume(userIdx = ${loginUserIdx}, query = ${req.query})`
+        `${LOG_TAG} getRecentPerfume(userIdx = ${loginUserIdx}, query = ${JSON.stringify(
+            req.query
+        )})`
     );
     Perfume.recentSearch(loginUserIdx, pagingRequestDTO.toPageDTO())
         .then((result: ListAndCountDTO<PerfumeThumbDTO>) => {
@@ -420,7 +428,9 @@ const recommendPersonalPerfume: RequestHandler = (
         req.query
     );
     logger.debug(
-        `${LOG_TAG} recommendPersonalPerfume(userIdx = ${loginUserIdx}, query = ${req.query})`
+        `${LOG_TAG} recommendPersonalPerfume(userIdx = ${loginUserIdx}, query = ${JSON.stringify(
+            req.query
+        )})`
     );
     const pagingDTO: PagingDTO = pagingRequestDTO.toPageDTO();
     return (
@@ -532,7 +542,9 @@ const recommendCommonPerfume: RequestHandler = (
     const ageGroup: number = Math.floor(req.query.age / 10) * 10;
     const gender: number = GenderMap[req.query.gender];
     logger.debug(
-        `${LOG_TAG} recommendCommonPerfume(userIdx = ${loginUserIdx}, query = ${req.query})`
+        `${LOG_TAG} recommendCommonPerfume(userIdx = ${loginUserIdx}, query = ${JSON.stringify(
+            req.query
+        )})`
     );
     const pagingDTO: PagingDTO = pagingRequestDTO.toPageDTO();
     Perfume.recommendByUser(loginUserIdx, pagingDTO, ageGroup, gender)
@@ -692,7 +704,9 @@ const getNewPerfume: RequestHandler = (
         req.query
     );
     logger.debug(
-        `${LOG_TAG} getNewPerfume(userIdx = ${loginUserIdx}, query = ${req.query})`
+        `${LOG_TAG} getNewPerfume(userIdx = ${loginUserIdx}, query = ${JSON.stringify(
+            req.query
+        )})`
     );
     Perfume.getNewPerfume(loginUserIdx, pagingRequestDTO.toPageDTO())
         .then((result: ListAndCountDTO<PerfumeThumbDTO>) => {
@@ -770,7 +784,9 @@ const getLikedPerfume: RequestHandler = (
         req.query
     );
     logger.debug(
-        `${LOG_TAG} getLikedPerfume(userIdx = ${userIdx}, loginUserIdx = ${loginUserIdx}, query = ${req.query})`
+        `${LOG_TAG} getLikedPerfume(userIdx = ${userIdx}, loginUserIdx = ${loginUserIdx}, query = ${JSON.stringify(
+            req.query
+        )})`
     );
     if (loginUserIdx != userIdx) {
         res.status(StatusCode.FORBIDDEN).json(
