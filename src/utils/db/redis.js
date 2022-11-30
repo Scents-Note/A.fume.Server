161701af04fe27294264d636c6d9e57d60313798
조하담 <hadamcho97@gmail.com>
2022-11-30 15:23:30 +0900

    [Fix] Redis 환경설정 변경 NODE_ENV에 따라 다른 database ID 지정

--- src/utils/db/redis.js
+++ src/utils/db/redis.js
@@ -5,12 +5,31 @@ dotenv.config();
 let redisClient;
 
 try {
+    let host;
+    let db;
+
     if (process.env.NODE_LOCATION === process.env.LOCATION_AWS) {
-        redisClient = createClient({ host : process.env.REDIS_HOST_PROD, port: process.env.REDIS_PORT})
+        host = process.env.REDIS_HOST_PROD
     }
     else {
-        redisClient = createClient({ host : 'localhost', port: process.env.REDIS_PORT}) // 로컬에서 작업시 로컬 레디스 사용
+        host = 'localhost'
     }
+
+    if (process.env.NODE_ENV === 'production') {
+        db =  process.env.REDIS_DB_ID_PROD
+    } else if (process.env.NODE_ENV === 'test') {
+        db = process.env.REDIS_DB_ID_TEST
+    } else {
+        db = process.env.REDIS_DB_ID_DEV
+    }
+
+    const config = {
+        host,
+        port: process.env.REDIS_PORT,
+        db
+    }
+    redisClient = createClient(config);
+
     console.log(`Successfully connected to ${process.env.NODE_LOCATION} Redis`)
 }
 catch (err) {
