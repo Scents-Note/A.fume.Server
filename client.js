3853fc0baced223606914789a394b44b45117ba8
조하담 <hadamcho97@gmail.com>
2022-12-01 14:25:50 +0900

    Update Client.js Apply async-await

--- client.js
+++ client.js
@@ -1,35 +1,28 @@
 const redis = require("redis");
+require("dotenv").config();
 
 // Creates a new Redis client
 // If REDIS_HOST is not set, the default host is localhost
 // If REDIS_PORT is not set, the default port is 6379
 const redisClient = redis.createClient({
-  host: 'redis',
+  host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT  
 });
 
-redisClient.connect()
+redisClient.connect().then( async () => {
+  console.log('redis connected');
 
-redisClient.on("error", function(err) {
-    console.log("Error " + err);
+  // Sets the key "octocat" to a value of "Mona the octocat"
+  await redisClient.set("octocat", "Mona the Octocat");
+  // Sets a key to "octocat", field to "species", and "value" to "Cat and Octopus"
+  await redisClient.hSet("species", "octocat", "Cat and Octopus");
+  // Sets a key to "octocat", field to "species", and "value" to "Dinosaur and Octopus"
+  await redisClient.hSet("species", "dinotocat", "Dinosaur and Octopus");
+  // Sets a key to "octocat", field to "species", and "value" to "Cat and Robot"
+  await redisClient.hSet("species", "robotocat", "Cat and Robot");
+
+  redisClient.quit();
+})
+.catch((error) => {
+  console.log("Error " + error);
 });
-
-// Sets the key "octocat" to a value of "Mona the octocat"
-redisClient.set("octocat", "Mona the Octocat");
-// Sets a key to "octocat", field to "species", and "value" to "Cat and Octopus"
-redisClient.hSet("species", "octocat", "Cat and Octopus");
-// Sets a key to "octocat", field to "species", and "value" to "Dinosaur and Octopus"
-redisClient.hSet("species", "dinotocat", "Dinosaur and Octopus");
-// Sets a key to "octocat", field to "species", and "value" to "Cat and Robot"
-redisClient.hSet("species", "robotocat", "Cat and Robot");
-
-redisClient.quit();
-
-// Gets all fields in "species" key
-// redisClient.hKeys("species", function (err, replies) {
-//     console.log(replies.length + " replies:");
-//     replies.forEach(function (reply, i) {
-//         console.log("    " + i + ": " + reply);
-//     });
-//     redisClient.quit();
-// });
