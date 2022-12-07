af65ee69db2685f9d80550158efae1963f009a0a
조하담 <hadamcho97@gmail.com>
2022-12-08 00:26:53 +0900

    Update client2.js Fix createClient config for redis version 4

--- client2.js
+++ client2.js
@@ -4,7 +4,7 @@ const redis = require("redis");
 // If REDIS_HOST is not set, the default host is localhost
 // If REDIS_PORT is not set, the default port is 6379
 const redisClient = redis.createClient({
-    host: 'redis',
+    url: 'redis://redis:6379',
     legacyMode: true
 });
 
