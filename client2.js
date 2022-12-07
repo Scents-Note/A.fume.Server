bd5b3034b2fb8ea51e6dda0a288e81b6a9224fac
조하담 <hadamcho97@gmail.com>
2022-12-07 23:50:33 +0900

    Update client2.js Try to use redis.print in node version 10

--- client2.js
+++ client2.js
@@ -4,30 +4,34 @@ const redis = require("redis");
 // If REDIS_HOST is not set, the default host is localhost
 // If REDIS_PORT is not set, the default port is 6379
 const redisClient = redis.createClient({
-    url: 'redis://redis:6379',
     legacyMode: true
 });
-
-redisClient.connect()
-
+redisClient.on('connect', () => {
+    console.info('Redis connected!');
+ });
 redisClient.on("error", function(err) {
     console.log("Error " + err);
 });
 
-// Sets the key "octocat" to a value of "Mona the octocat"
-redisClient.set("octocat", "Mona the Octocat", redis.print);
-// Sets a key to "octocat", field to "species", and "value" to "Cat and Octopus"
-redisClient.hset("species", "octocat", "Cat and Octopus", redis.print);
-// Sets a key to "octocat", field to "species", and "value" to "Dinosaur and Octopus"
-redisClient.hset("species", "dinotocat", "Dinosaur and Octopus", redis.print);
-// Sets a key to "octocat", field to "species", and "value" to "Cat and Robot"
-redisClient.hset(["species", "robotocat", "Cat and Robot"], redis.print);
-// Gets all fields in "species" key
+redisClient
+    .connect()
+    .then(function(){
+        // Sets the key "octocat" to a value of "Mona the octocat"
+        redisClient.set("octocat", "Mona the Octocat", redis.print);
+        // Sets a key to "octocat", field to "species", and "value" to "Cat and Octopus"
+        redisClient.hset("species", "octocat", "Cat and Octopus", redis.print);
+        // Sets a key to "octocat", field to "species", and "value" to "Dinosaur and Octopus"
+        redisClient.hset("species", "dinotocat", "Dinosaur and Octopus", redis.print);
+        // Sets a key to "octocat", field to "species", and "value" to "Cat and Robot"
+        redisClient.hset(["species", "robotocat", "Cat and Robot"], redis.print);
+        // Gets all fields in "species" key
 
-redisClient.hkeys("species", function (err, replies) {
-    console.log(replies.length + " replies:");
-    replies.forEach(function (reply, i) {
-        console.log("    " + i + ": " + reply);
-    });
-    redisClient.quit();
-});
+        redisClient.hkeys("species", function (err, replies) {
+            console.log(replies.length + " replies:");
+            replies.forEach(function (reply, i) {
+                console.log("    " + i + ": " + reply);
+            });
+            redisClient.quit();
+        })
+    }
+);
