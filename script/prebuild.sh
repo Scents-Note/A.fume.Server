026573cd799d79fb1699463a9cdfd3807ab8b705
Heesung Youn <heesung6701@naver.com>
2023-01-13 01:24:08 +0900

    extract global install script from prebuild.sh to setup-global.sh

--- script/prebuild.sh
+++ script/prebuild.sh
@@ -3,11 +3,3 @@
 pm2 stop ecosystem.json
 
 npm ci
-
-npm install -g pm2
-npm install -g mocha
-npm install -g typescript
-npm install -g ts-node
-npm install -g tsconfig-paths
-
-npm audit fix
---
+++ script/setup-global.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+npm install -g pm2
+npm install -g mocha
+npm install -g typescript
+npm install -g ts-node
+npm install -g tsconfig-paths
