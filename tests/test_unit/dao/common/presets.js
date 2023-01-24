a2926f3ff3091cf8c8d5362eb7c0eeb032e4e2ca
hee.youn <heesung6701@naver.com>
2023-01-24 18:50:25 +0900

    modify to run presets.js only first time

--- tests/test_unit/dao/common/presets.js
+++ tests/test_unit/dao/common/presets.js
@@ -5,29 +5,33 @@ if (properties.NODE_ENV != 'test') {
     throw new Error('Only allow TEST ENV');
 }
 
+let firstTime = true;
+
 module.exports = async (context) => {
-    context.timeout(100000);
     if (properties.NODE_ENV != 'test') {
         throw new Error('Only allow TEST ENV');
     }
-    await Promise.all([
-        sequelize
-            .query('SET FOREIGN_KEY_CHECKS = 0')
-            .then(function () {
-                return sequelize.sync({ force: true });
-            })
-            .then(function () {
-                return sequelize.query('SET FOREIGN_KEY_CHECKS = 1');
-            }),
-        require('@src/utils/db/mongoose.js'),
-    ]);
-    await require('./seeds.js')();
-    await sequelize.query(
-        'CREATE FULLTEXT INDEX ft_idx_perfume_name ON perfumes(`name`, `english_name`)'
-    );
-    await sequelize.query(
-        'CREATE FULLTEXT INDEX ft_idx_brand_name ON brands(`name`, `english_name`)'
-    );
-
-    context.timeout();
+    if (firstTime) {
+        context.timeout(100000);
+        await Promise.all([
+            sequelize
+                .query('SET FOREIGN_KEY_CHECKS = 0')
+                .then(function () {
+                    return sequelize.sync({ force: firstTime });
+                })
+                .then(function () {
+                    return sequelize.query('SET FOREIGN_KEY_CHECKS = 1');
+                }),
+            require('@src/utils/db/mongoose.js'),
+        ]);
+        await require('./seeds.js')();
+        await sequelize.query(
+            'CREATE FULLTEXT INDEX ft_idx_perfume_name ON perfumes(`name`, `english_name`)'
+        );
+        await sequelize.query(
+            'CREATE FULLTEXT INDEX ft_idx_brand_name ON brands(`name`, `english_name`)'
+        );
+        firstTime = false;
+        context.timeout();
+    }
 };
